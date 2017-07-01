518d90c0a07b247e2fd3a469531fa09f376d6b26
Titus Tienaah <titus.intdxdt@gmail.com>
2017-07-01 06:24:14 -0300

    robust seg intersect

---
+++ robust_segment_intersect/__init__.py
@@ -0,0 +1,36 @@
+from rubust_orient import orientation2d
+
+def segments_intersect(a0, a1, b0, b1):
+    x0 = orientation2d(a0, b0, b1)
+    y0 = orientation2d(a1, b0, b1)
+
+    if (x0 > 0 and y0 > 0) or (x0 < 0 and y0 < 0):
+        return False
+
+    x1 = orientation2d(b0, a0, a1)
+    y1 = orientation2d(b1, a0, a1)
+
+    if (x1 > 0 and y1 > 0) or (x1 < 0 and y1 < 0):
+        return False
+
+    # check for degenerate collinear case
+    if x0 == 0 and y0 == 0 and x1 == 0 and y1 == 0:
+        return check_collinear(a0, a1, b0, b1)
+
+    return True
+
+def check_collinear(a0, a1, b0, b1):
+    for d in xrange(0, 2):
+        x0 = a0[d]
+        y0 = a1[d]
+        l0 = min(x0, y0)
+        h0 = max(x0, y0)
+
+        x1 = b0[d]
+        y1 = b1[d]
+        l1 = min(x1, y1)
+        h1 = max(x1, y1)
+
+        if h1 < l0 or h0 < l1:
+            return False
+    return True
---
+++ robust_segment_intersect/test.py
@@ -0,0 +1,21 @@
+import unittest
+from robust_segment_intersect import segments_intersect as crosses
+
+
+class TestRobustSegIntersect(unittest.TestCase):
+    def test_seg_intersect(self):
+        self.assertTrue(crosses([-1, 0], [1, 0], [0, -1], [0, 1]))  # general test
+        self.assertFalse(crosses([0.5, 0], [1, 0], [0, -1], [0, 1]))
+        self.assertTrue(crosses([0, 0], [1, 0], [0, -1], [0, 1]))
+        self.assertTrue(crosses([0, 0], [100000000000000020000, 1e-12], [1, 0], [1e20, 1e-11]))
+        self.assertFalse(crosses([0, 0], [1e20, 1e-11], [1, 0], [100000000000000020000, 1e-12]))
+
+        self.assertFalse(crosses([0, 1], [0, 2], [0, -1], [0, -2]))  # collinear, no intersect
+        self.assertTrue(crosses([0, 1], [0, 2], [0, 1.5], [0, -2]))  # collinear, intersect
+
+        self.assertTrue(crosses([0, 1], [0, 2], [0, 1], [0, -2]))  # collinear, endpoint touch
+        self.assertTrue(crosses([0, 1], [0, -1], [0, 0], [0, 1]))  # endpoint touches
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestRobustSegIntersect)
+unittest.TextTestRunner(verbosity=4).run(suite)
